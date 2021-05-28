472dbdad97658507dddf22aac593946544deda7b
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-05-28 13:49:27 -0500

    create the proyect chat

---
+++ chat/lib/pages/loading_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoagindPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('LoagindPage'),
+        ),
+      ),
+    );
+  }
+}
