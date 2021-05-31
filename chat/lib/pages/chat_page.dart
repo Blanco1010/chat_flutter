9cbbfcb05e009bf36b09791d35df9d8e129d0715
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-05-31 09:34:25 -0500

    add the bututon for chat

--- chat/lib/pages/chat_page.dart
+++ chat/lib/pages/chat_page.dart
@@ -1,6 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  final _focusNode = new FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,22 +42,65 @@ class ChatPage extends StatelessWidget {
         elevation: 1,
       ),
       body: Container(
-          child: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              physics: BouncingScrollPhysics(),
-              itemBuilder: (_, i) => Text('$i'),
-              reverse: true,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              ),
             ),
-          ),
-          Divider(height: 1),
-          Container(
-            color: Colors.white,
-            height: 100,
-          ),
-        ],
-      )),
+            Divider(height: 1),
+            Container(
+              color: Colors.white,
+              child: _inputChat(),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String texto) {},
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Enviar Mensaje'),
+                focusNode: _focusNode,
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Platform.isIOS
+                    ? CupertinoButton(
+                        child: Text('Enviar'),
+                        onPressed: () {},
+                      )
+                    : Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: IconButton(
+                          icon: Icon(Icons.send, color: Colors.blue[300]),
+                          onPressed: () {},
+                        ),
+                      )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSubmit(String text) {
+    print('$text');
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
