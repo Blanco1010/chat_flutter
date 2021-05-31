d9dd16eddaa564005d62d02f60575ba86a2a20c9
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-05-31 09:12:24 -0500

    add the code for chat

--- chat/lib/pages/chat_page.dart
+++ chat/lib/pages/chat_page.dart
@@ -4,11 +4,49 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text('ChatPage'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: Text(
+                'Te',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Text(
+              'Melissa Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
         ),
+        centerTitle: true,
+        elevation: 1,
       ),
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            ),
+          ),
+          Divider(height: 1),
+          Container(
+            color: Colors.white,
+            height: 100,
+          ),
+        ],
+      )),
     );
   }
 }
