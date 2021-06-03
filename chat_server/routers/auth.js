f4a2dc21a37f08528f4cd9067c67f7a4ce36ed0f
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-06-03 12:27:15 -0500

    add the code for verify when creating the user

--- chat_server/routers/auth.js
+++ chat_server/routers/auth.js
@@ -6,12 +6,15 @@ path: api/login
 
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario } = require('../controllers/auth');
 
 const router = Router();
 
 
-router.post('/new',crearUsuario);
+router.post('/new',[
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+] ,crearUsuario);
 
 
 
