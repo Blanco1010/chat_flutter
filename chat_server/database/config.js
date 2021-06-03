6e954e846d85e35c23b374c11cc5b1e1a7f86eaf
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-06-03 11:36:43 -0500

    add the code JS

--- chat_server/database/config.js
+++ chat_server/database/config.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+        });
 
-        console.log('init db config');
+        console.log('DB Online');
+        
 
     } catch (error){
         console.log('Error');
